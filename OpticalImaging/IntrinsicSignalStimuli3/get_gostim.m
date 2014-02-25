88fe491b1abea88da1c7acc2d5911faabb356da9
heimel <heimel@nin.knaw.nl>
2014-02-25 14:05:47 +0100

    fixed arduino parallelport for andrewstim

--- OpticalImaging/IntrinsicSignalStimuli3/get_gostim.m
+++ OpticalImaging/IntrinsicSignalStimuli3/get_gostim.m
@@ -4,6 +4,11 @@ function [go, stim] = get_gostim(lpt)
 %   [GO, STIM] = GET_GOSTIM(LPT)
 %   2004-2013, Alexander Heimel
 
+if nargin<1
+    disp('GET_GOSTIM: No lpt given. Call lpt=open_parallelport first.');
+    return
+end
+
 go = 0;
 stim = 0;
 
@@ -12,15 +17,22 @@ switch class(lpt)
         try
             fopen(lpt);
         end
-      %  [status,count] = fscanf('%d',lpt);
-      readasync(lpt);status= fread(lpt)
-%status
- %       fclose(lpt); 
-        
-    %    status = str2num(status);
-        stim = bitand(status,2^7-1); % remove bit 7 (GO bit)
-        go = (bitand(status,2^7)>0);
-
+        readasync(lpt);status = fread(lpt);
+        switch lower(host)
+            case 'andrewstim'
+                go = (bitand(status,8)==8);
+                stim = 0;
+                stim = stim + (bitand(status,1)==1);
+                stim = stim + 2*(bitand(status,128)==128);
+                stim = stim + 4*(bitand(status,4)==4);
+                stim = stim + 8*(bitand(status,2)==2);
+                stim = stim + 16*(bitand(status,16)==16);
+                stim = stim + 32*(bitand(status,64)==64);
+                stim = stim + 64*(bitand(status,32)==32);
+            otherwise % 
+                stim = bitand(status,2^7-1); % remove bit 7 (GO bit)
+                go = (bitand(status,2^7)>0);
+        end
     otherwise % assume lpt
         status = lpt.read;
         stim = bitand(status,2^7-1); % remove bit 7 (GO bit)
