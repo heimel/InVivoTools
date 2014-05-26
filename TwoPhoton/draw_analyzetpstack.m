2e0eac65ad8d0e6fc5a1c2eec73b9a83f56ce502
Alexander Heimel <heimel@nin.knaw.nl>
2014-05-26 12:23:57 +0200

    snapping to an arbitrary number of channels

--- TwoPhoton/draw_analyzetpstack.m
+++ TwoPhoton/draw_analyzetpstack.m
@@ -238,7 +238,8 @@ guicreate(txt,'String','px','Enable','on','width','auto','parent',hroilabelspane
 guicreate(button,'String','Neurite','Tag','drawNeuriteBt','left','left','tooltipstring','Draw neurite','Enable','on','width','auto','parent',hroilabelspanel,'move','right');
 % Snap to
 guicreate(txt,'String','Snap','Enable','on','width','auto','parent',hroilabelspanel,'move','right');
-guicreate(popup,'String',{'','1','2','3'},'Tag','snaptoPopup','Enable','on','width',30,'parent',hroilabelspanel,'move','right','callback','genercallback');
+guicreate(popup,'String',['no' cellfun(@num2str,num2cell(1:ti.NumberOfChannels,[ti.NumberOfChannels 1]),...
+    'UniformOutput',false)],'Tag','snaptoPopup','Enable','on','width',35,'parent',hroilabelspanel,'move','right','callback','genercallback');
 
 % Export
 guicreate(button,'String','Export','Tag','exportROIsBt','Enable','on','tooltipstring','Export ROIs','width','auto','parent',hroilabelspanel,'move','down');
