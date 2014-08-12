e249ae46ecc9594e370ff504c8869d4c19fa99b1
Alexander Heimel <heimel@nin.knaw.nl>
2014-08-12 11:48:12 +0200

    added changed_process_parameters

---
+++ ExpDataTools/changed_process_parameters.m
@@ -0,0 +1,34 @@
+function changed_process_parameters(params,oldparams)
+%CHANGED_PROCESS_PARAMETERS checks for local overrides of changed parameters
+%
+%  CHANGED_PROCESS_PARAMETERS(PARAMS,OLDPARAMS)
+%
+% 2014, Alexander Heimel
+
+persistent mentioned
+
+[identical,flds] = structdiff(params,oldparams);
+if ~identical
+    %     if isempty(mentioned)
+    %         logmsg('Overriding process parameters with possible local settings');
+    %     end
+    for f = flds
+        if isfield(oldparams,f{1})
+            if isnumeric(params.(f{1}))
+                str = mat2str(params.(f{1}));
+            elseif logical(params.(f{1}))
+                if params.(f{1})
+                    str = 'true';
+                else
+                    str = 'false';
+                end
+            else
+                str = params.(f{1});
+            end
+            if isempty(mentioned)
+                logmsg(['Set ' f{1} '=' str]);
+            end
+        end
+    end
+    mentioned = true;
+end
