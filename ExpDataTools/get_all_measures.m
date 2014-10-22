2d31ae3d7d8f3209336ccd39ce4789d5a8d32ec7
heimel <heimel@nin.knaw.nl>
2014-10-22 09:38:13 +0200

    added get_all_measures

---
+++ ExpDataTools/get_all_measures.m
@@ -0,0 +1,52 @@
+function measures_db = get_all_measures
+
+h = waitbar(0,'Loading all measures');
+measures_db = [];
+
+datatypes = {'tp','ec','oi'};
+for i=1:length(datatypes)
+    measures_db = extract_db( measures_db, datatypes{i});
+end
+close(h);    
+if ~nargout
+    show_table(measures_db);
+end
+
+
+function measures_db = extract_db( measures_db, datatype)
+
+% get which database
+[testdb, experimental_pc] = expdatabases( datatype );
+
+[db,filename]=load_expdatabase( testdb,'',false,false,true );
+if isempty(db)
+    return
+end
+
+logmsg(['Extracting ' datatype]);
+measures = {};
+measures = extract_measures(db,measures);
+
+for i=1:length(measures)
+    measures_db(end+1).name = [datatype ':' measures{i}];
+    measures_db(end).label = measures{i};
+    measures_db(end).datatype = datatype;
+    measures_db(end).stim_type = '';
+    measures_db(end).point = '';
+    measures_db(end).measures = [datatype ':' measures{i}];
+end
+    
+
+
+
+
+function measures = extract_measures(db,measures)
+if nargin<1
+    measures = {};
+end
+
+for i=1:length(db)
+    if isstruct(db(i).measures)
+        measures = uniq(sort(cat(1,measures,fields(db(i).measures))))';
+    end
+end
