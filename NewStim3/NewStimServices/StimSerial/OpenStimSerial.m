96899e4cab7aaedf020905b6c9672c17913a7f4f
heimel <heimel@nin.knaw.nl>
2014-11-16 20:52:58 +0100

    replaced errordlg in OpenStimSerial

--- NewStim3/NewStimServices/StimSerial/OpenStimSerial.m
+++ NewStim3/NewStimServices/StimSerial/OpenStimSerial.m
@@ -1,29 +1,26 @@
-StimSerialGlobals;
+StimSerialGlobals
 
-if StimSerialSerialPort,
-    
+if StimSerialSerialPort
     if isempty(StimSerialScript)
         try
             StimSerialScript=StimSerial('Open',StimSerialScriptIn,StimSerialScriptOut,9600);
         catch me
             switch me.identifier
                 case 'MATLAB:serial:fopen:opfailed'
-                    errordlg(['Could not open serial ports ' ...
+                    errormsg(['Could not open serial ports ' ...
                         StimSerialScriptIn ' and/or ' StimSerialScriptOut ...
                         '. Check if names are correct. On linux they should be like /dev/ttySX and can be identified by ''sudo setsetial -bg /dev/ttyS*'' ' ...
                         'and on Windows COMX. The correct names should be entered in NewStimConfiguration. ' ...
-                        'Disabling serial port use.'],'OpenStimSerial');
-                    disp(['OPENSTIMSERIAL: ' me.message]);
-                    disp('OPENSTIMSERIAL: Check serial port settings in NewStimConfiguration. Disabling serial port use.');
+                        'Disabling serial port use.']);
                 otherwise
-                    errordlg(me.message)
+                    errormsg(me.message)
             end
             StimSerialSerialPort = 0;
             return
         end
     end
     
-    if isempty(StimSerialStim),
+    if isempty(StimSerialStim)
         if strcmp(StimSerialScriptIn,StimSerialStimIn)&&...
                 strcmp(StimSerialScriptOut,StimSerialStimOut),
             StimSerialStim = StimSerialScript;
@@ -33,19 +30,17 @@ if StimSerialSerialPort,
             catch me
                 switch me.identifier
                     case 'MATLAB:serial:fopen:opfailed'
-                        errordlg(['Could not open serial ports ' ...
+                        errormsg(['Could not open serial ports ' ...
                             StimSerialStimIn ' and/or ' StimSerialStimOut ...
                             '. Check if names are correct. On linux they should be like /dev/ttySX and can be identified by ''sudo setsetial -bg /dev/ttyS*'' ' ...
                             'and on Windows COMX. The correct names should be entered in NewStimConfiguration. ' ...
-                            'Disabling serial port use.'],'OpenStimSerial');
-                        disp(['OPENSTIMSERIAL: ' me.message]);
-                        disp('OPENSTIMSERIAL: Check serial port settings in NewStimConfiguration. Disabling serial port use.');
+                            'Disabling serial port use.']);
                     otherwise
-                        errordlg(me.message)
+                        errormsg(me.message)
                 end
                 StimSerialSerialPort = 0;
                 return
             end
-        end;
-    end;
-end;
+        end
+    end
+end
