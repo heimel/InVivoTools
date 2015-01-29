8a56ef1db48deed12ca5295473dfce9acc38f1eb
heimel <heimel@nin.knaw.nl>
2015-01-29 22:42:18 +0100

    avoid using savefig for macs

--- General/graphs/save_figure.m
+++ General/graphs/save_figure.m
@@ -3,6 +3,7 @@ function filename=save_figure(filename,path,h)
 %
 %  SAVE_FIGURE(FILENAME,PATH,H)
 %
+% 200X-2015, Alexander Heimel
 
 if nargin<2
     path='';
@@ -16,81 +17,29 @@ end
 
 filename=lower(subst_filechars(filename));
 
-if strcmp(filename(max(1,end-3):end),'.png')==1
-    filename=filename(1:end-4);
+if strcmp(filename(max(1,end-3):end),'.png')==1 
+    filename=filename(1:end-4); % remove png extension
 end
 
 path=compose_figurepath(path);
 
-if 0 & isunix % deprecated since arrival of savefig
-    
-    filename=fullfile(path,filename);
-    
-    v=version;
-    if v(1)=='5' % student version
-        saveas(h,[filename '.eps'],'psc2')
-        command=['!sed ''s/(Student Version of MATLAB) show//'' ' ...
-            filename '.eps > ' filename 'c.eps'];
-        eval(command);
-        command=['!ps2pdf ' filename 'c.eps ' filename 'c.pdf'];
-        eval(command);
-        
-        command=['!convert -trim ' filename 'c.pdf ' filename '.png'];
-        eval(command);
-        command=['! rm ' filename 'c.eps ' filename 'c.pdf ' filename '.eps' ];
-        eval(command);
-    else
-        % official version
-        % adjust paper size to fit figure
-        
-        
-        set(h,'PaperOrientation','portrait');
-        set(h,'PaperPositionMode','auto');
-        pp=get(h,'PaperPosition');
-        set(h, 'PaperSize', [pp(3) pp(4)]);
-        switch v(end-6:end-1)
-            case 'R2008a'
-                disp('version R2008a');
-                saveas(h,[filename 'c.pdf'],'pdf')
-                command=['!convert -trim ' filename 'c.pdf ' filename '.png'];
-                eval(command);
-                command=['! rm '  filename 'c.pdf '];
-                eval(command);
-            otherwise
-                saveas(h,[filename 'c.pdf'],'pdf')
-                command=['!pdf2ps ' filename 'c.pdf ' filename 'c.ps'];
-                eval(command);
-                command=['!convert -trim ' filename 'c.ps ' filename '.png'];
-                eval(command)
-                command=['! rm '  filename 'c.pdf ' filename 'c.ps '];
-                eval(command);
-        end
-    end
-    
-    
-else % windows pc or recent matlab linux
-    savewd=pwd;
-    if ~exist(path,'dir')
-        disp(['SAVE_FIGURE: ' path ' does not exist. Saving to desktop']);
-        path = getdesktopfolder;
-    end        
-    cd(path);
-    if isunix
-        savefig([filename '.png'],h,'png');
-        %        saveas(h,[filename '.eps'],'epsc2');
-        %        saveas(h,[filename '.fig'],'fig');
-        
-        %disp('SAVE_FIGURE: Turned off svg save.');
-        if 0
-            plot2svg( [filename '.svg'],h,'png');
-        end
-    else % windows (assume absence of ghostscript)
-        saveas(h,[filename '.png'],'png');
-    end
-    filename=fullfile(path,filename);
-    cd(savewd);
+savewd=pwd;
+if ~exist(path,'dir')
+    logmsg([ path ' does not exist. Saving to desktop']);
+    path = getdesktopfolder;
 end
-
+cd(path);
+if  isunix && ~ismac
+    savefig([filename '.png'],h,'png'); % to avoid missing fonts
+    % alternative
+    % frame = getframe(h);
+    % imwrite(frame.cdata,[filename '.png']);
+else % windows or max (assume absence of ghostscript)
+    saveas(h,[filename '.png'],'png');
+    saveas(h,[filename '.eps'],'eps');
+end
+filename = fullfile(path,filename);
+cd(savewd);
 
 filename=[filename '.png'];
-disp(['SAVE_FIGURE: Figure saved as ' filename]);
+logmsg(['Figure saved as ' filename ' (bitmap) and as eps (vector graphics)']);
