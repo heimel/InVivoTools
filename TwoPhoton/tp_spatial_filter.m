5c11559ea302ed70d78e3084cf3bfe98a2808064
heimel <heimel@nin.knaw.nl>
2015-02-08 14:34:54 +0100

    extended verbose implementation for tp analysis

--- TwoPhoton/tp_spatial_filter.m
+++ TwoPhoton/tp_spatial_filter.m
@@ -1,52 +1,65 @@
-function im = tp_spatial_filter( im, filtername, options )
+function im = tp_spatial_filter( im, filtername, options, verbose )
 %TP_SPATIAL_FILTER applies spatial filter to 2D image
 %
-%  IM = TP_SPATIAL_FILTER( IM, FILTERNAME )
+%  IM = TP_SPATIAL_FILTER( IM, FILTERNAME, VERBOSE )
 %
-% 2011, Alexander Heimel
+% 2011-2015, Alexander Heimel
 %
+
+if nargin<4
+    verbose = [];
+end
+if isempty(verbose)
+    verbose = true;
+end
 
 if nargin<2
     filtername = 'medfilt2';
     options = '';
-    
-    % filtername = 'wiener2';
-    % options = '[5 5]';
 end
 if ~isempty(options)
     options = [',' options];
 end
 
-waiting = 0;
-wait_interval = 1/3/size(im,4);
-hbar = waitbar(waiting,['Applying spatial filter ' filtername ]);
-
+if verbose
+    waiting = 0;
+    wait_interval = 1/3/size(im,4);
+    hbar = waitbar(waiting,['Applying spatial filter ' filtername ]);
+end
 
 n_channels = size(im,4);
 
 % applying filter
-for ch = 1:size(im,4)
+for ch = 1:n_channels
     for fr = 1:size(im,3)
         eval(['im(:,:,fr,ch) = ' filtername '(squeeze(im(:,:,fr,ch))' options ');']);
     end
-    waiting = waiting + wait_interval;
-    waitbar(waiting,hbar);
+    if verbose
+        waiting = waiting + wait_interval;
+        waitbar(waiting,hbar);
+    end
 end
 if ndims(im)>3
-    for ch = 1:size(im,4)
+    for ch = 1:n_channels
         for x = 1:size(im,1)
             eval(['im(x,:,:,ch) = ' filtername '(squeeze(im(x,:,:,ch))' options ');']);
         end
-        waiting = waiting + wait_interval;
-        waitbar(waiting,hbar);
+        if verbose
+            waiting = waiting + wait_interval;
+            waitbar(waiting,hbar);
+        end
     end
-    for ch = 1:size(im,4)
+    for ch = 1:n_channels
         for y = 1:size(im,2)
             eval(['im(:,y,:,ch) = ' filtername '(squeeze(im(:,y,:,ch))' options ');']);
         end
-        waiting = waiting + wait_interval;
-        waitbar(waiting,hbar);
+        if verbose
+            waiting = waiting + wait_interval;
+            waitbar(waiting,hbar);
+        end
     end
 end
-close(hbar);
+if verbose
+    close(hbar);
+end
 
