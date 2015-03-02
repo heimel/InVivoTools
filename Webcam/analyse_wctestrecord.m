79caf014b04ff14f605cc8c0a1ec684206be6791
heimel <heimel@nin.knaw.nl>
2015-03-02 16:54:28 +0100

    working on wc analysis

--- Webcam/analyse_wctestrecord.m
+++ Webcam/analyse_wctestrecord.m
@@ -15,5 +15,27 @@ end
 
 d = dir(fullfile(experimentpath(record),'webcam*info.mat'));
 for i = 1:length(d)
-    wcinfo(i) = load(fullfile(experimentpath(record),d(i).name))
+    wcinfo(i) = load(fullfile(experimentpath(record),d(i).name));
 end
+
+% create mp4 wrappers
+for i=1:length(d)
+    wcinfo(i).mp4name = [ wcinfo(i).filename '.mp4'];
+    if  ~exist(wcinfo(i).mp4name,'file')
+        if isunix
+            [stat,output ] = system(['MP4Box -fps 30 -add ' wcinfo(i).filename ' ' wcinfo(i).mp4name])
+        else
+            logmsg(['Cannot create mp4 wrapper for ' wcinfo(i).filename '. Try on linux computer.']);
+        end
+    end
+end
+
+for i=1:length(wcinfo)
+    logmsg(['Recorded in ' wcinfo(i).filename]);
+    logmsg(['Stimulus started: ' num2str(wcinfo(i).stimstart)]);
+end
+
+
+
+player = 'totem';
+[status,out] = system([ player ' ' wcinfo(1).mp4name ]);
