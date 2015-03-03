904dc02214dabc569230a8939ce29f8ddf36476d
heimel <heimel@nin.knaw.nl>
2015-03-03 01:03:49 +0100

    worked on location of webcam files

--- Webcam/analyse_wctestrecord.m
+++ Webcam/analyse_wctestrecord.m
@@ -13,29 +13,37 @@ if isempty(verbose)
     verbose = true;
 end
 
+
+
 d = dir(fullfile(experimentpath(record),'webcam*info.mat'));
+logmsg(['Found ' num2str(length(d)) ' webcam records in ' experimentpath(record)]);
 for i = 1:length(d)
     wcinfo(i) = load(fullfile(experimentpath(record),d(i).name));
+    
+    [pth,wcinfo(i).filename] = fileparts(wcinfo(i).filename);
+    wcinfo(i).filename = [wcinfo(i).filename '.h264'];
 end
 
 % create mp4 wrappers
+parpath = fullfile(experimentpath(record),'..');
 for i=1:length(d)
     wcinfo(i).mp4name = [ wcinfo(i).filename '.mp4'];
     if  ~exist(wcinfo(i).mp4name,'file')
         if isunix
-            [stat,output ] = system(['MP4Box -fps 30 -add ' wcinfo(i).filename ' ' wcinfo(i).mp4name])
+            cmd = ['MP4Box -fps 30 -add "' fullfile(parpath,wcinfo(i).filename) '" "' fullfile(parpath,wcinfo(i).mp4name) '"']
+            [stat,output ] = system(cmd)
         else
-            logmsg(['Cannot create mp4 wrapper for ' wcinfo(i).filename '. Try on linux computer.']);
+            logmsg(['Cannot create mp4 wrapper for ' fullfile(parpath,wcinfo(i).filename) '. Try on linux computer, or run sudo apt-get -y install gpac']);
         end
     end
 end
 
 for i=1:length(wcinfo)
-    logmsg(['Recorded in ' wcinfo(i).filename]);
+    logmsg(['Recorded in ' fullfile(parpath,wcinfo(i).filename)]);
     logmsg(['Stimulus started: ' num2str(wcinfo(i).stimstart)]);
 end
 
 
-
-player = 'totem';
-[status,out] = system([ player ' ' wcinfo(1).mp4name ]);
+% 
+% player = 'totem';
+% [status,out] = system([ player ' ' wcinfo(1).mp4name ]);
