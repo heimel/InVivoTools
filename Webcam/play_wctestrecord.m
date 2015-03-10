6d585cd0e7cdc11a955d11ed424f5d2f840de783
heimel <heimel@nin.knaw.nl>
2015-03-10 13:01:34 +0100

    enabled vlc player in unix

--- Webcam/play_wctestrecord.m
+++ Webcam/play_wctestrecord.m
@@ -4,7 +4,12 @@ function play_wctestrecord(record)
 % 2015, Alexander Heimel
 
 if isunix
-    player = 'totem';
+    [status,out] = system('which vlc');
+    if status==0
+        player = 'vlc' ;
+    else
+        player = 'totem';
+    end
 else
     errormsg('Do not know which player to use for Windows. Needs to be implemented.');
     return
@@ -12,5 +17,17 @@ end
 
 wcinfo = wc_getmovieinfo( record);
 
+
 rec = 1;
-[status,out] = system([ player ' ' fullfile(    wcinfo(rec).path,wcinfo(rec).mp4name) ]);
+switch player
+    case 'vlc'
+        player = [ player ' --start-time=' num2str(wcinfo(rec).stimstart)];
+end
+
+cmd = [ player ' ' fullfile(    wcinfo(rec).path,wcinfo(rec).mp4name) ];
+
+[status,out] = system(cmd);
+%[status,out] = unix([ player ' ' fullfile(    wcinfo(rec).path,wcinfo(rec).mp4name) ]);
+
+status
+out
--- Webcam/wc_getmovieinfo.m
+++ Webcam/wc_getmovieinfo.m
@@ -26,12 +26,19 @@ for i=1:length(d)
     wcinfo(i).mp4name = [ wcinfo(i).filename '.mp4'];
     if  1 || ~exist(fullfile(parpath,wcinfo(i).mp4name),'file')
         if isunix
+            if exist(fullfile(parpath,wcinfo(i).mp4name),'file')
+                logmsg(['Backing up ' fullfile(wcinfo(i).path,wcinfo(i).mp4name)]);
+                movefile(fullfile(wcinfo(i).path,wcinfo(i).mp4name),fullfile(wcinfo(i).path ,[wcinfo(i).mp4name '.bak']));
+            end
+            logmsg(['Creating mp4 wrapper ' fullfile(parpath,wcinfo(i).filename)]);
             cmd = ['MP4Box -fps 30 -add "' fullfile(parpath,wcinfo(i).filename) '" "' fullfile(parpath,wcinfo(i).mp4name) '"'];
             [stat,output ] = system(cmd);
             if stat==0
                 logmsg(['Created mp4 wrapper ' fullfile(parpath,wcinfo(i).filename)]);
             elseif stat==127
                 logmsg(['Cannot create mp4 wrapper for ' fullfile(parpath,wcinfo(i).filename) '. Run sudo apt-get -y install gpac']);
+            else
+                logmsg(['Cannot create mp4 wrapper for ' fullfile(parpath,wcinfo(i).filename) '. ' output]);
             end
         else
             logmsg(['Cannot create mp4 wrapper for ' fullfile(parpath,wcinfo(i).filename) '. Try on linux computer, or run sudo apt-get -y install gpac']);
