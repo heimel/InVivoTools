3015d2b6e030ab4f297f3e9d2a27a4490eb9d404
Alexander Heimel <heimel@nin484.herseninstituut.knaw.nl>
2015-03-10 13:50:37 +0100

    move replay possible on windows

--- Webcam/play_wctestrecord.m
+++ Webcam/play_wctestrecord.m
@@ -3,31 +3,30 @@ function play_wctestrecord(record)
 %
 % 2015, Alexander Heimel
 
-if isunix
-    [status,out] = system('which vlc');
-    if status==0
-        player = 'vlc' ;
-    else
-        player = 'totem';
-    end
-else
-    errormsg('Do not know which player to use for Windows. Needs to be implemented.');
+par = wcprocessparams( record );
+
+if isempty(par.wc_playercommand)
+    errormsg('No videoplayer found. Add line to processparms_local.m with par.wc_playercommand to set player.');
     return
 end
 
 wcinfo = wc_getmovieinfo( record);
 
-
 rec = 1;
-switch player
+starttime = (wcinfo(rec).stimstart-par.wc_playbackpretime) * 1.015;
+cmd = par.wc_playercommand;
+switch par.wc_player
     case 'vlc'
-        player = [ player ' --start-time=' num2str(wcinfo(rec).stimstart)];
+        cmd = [ cmd ' --start-time=' num2str(starttime)];
 end
 
-cmd = [ player ' ' fullfile(    wcinfo(rec).path,wcinfo(rec).mp4name) ];
+cmd = [ cmd ' ' fullfile(    wcinfo(rec).path,wcinfo(rec).mp4name) ];
+
+switch par.wc_player
+    case 'vlc'
+        logmsg('Press ''p'' to replay.');
+end
 
 [status,out] = system(cmd);
-%[status,out] = unix([ player ' ' fullfile(    wcinfo(rec).path,wcinfo(rec).mp4name) ]);
 
-status
 out
