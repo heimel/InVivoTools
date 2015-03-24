b89f0b338f6c87777593000e4a8e2359ca6eca69
heimel <heimel@nin.knaw.nl>
2015-03-24 12:25:10 +0100

    fixed polar orientation plots

--- ExpDataTools/results_ectestrecord.m
+++ ExpDataTools/results_ectestrecord.m
@@ -282,7 +282,11 @@ for c=1:n_cells
                     plot_polar_curve(measure,'-');
                     switch lower(record.setup)
                         case {'nin380','nori001','antigua'}
-                            set(gca,'xdir','reverse' ); 
+                            % do nothing for backprojection screen
+                            %set(gca,'ydir','reverse' ); 
+                        otherwise
+                            set(gca,'ydir','reverse' ); 
+                            
                     end
                     
                     hold on
