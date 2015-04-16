431a449ca056c147d37e35b0ee6a5275c550c12a
chrisvdt <cvandert@gmail.com>
2015-04-16 16:35:23 +0200

    initial bouton analysis
    finding relative bouton positions along an axon

--- TwoPhoton/tp_automated_bouton_analysis.m
+++ TwoPhoton/tp_automated_bouton_analysis.m
@@ -1,15 +1,60 @@
 function record = tp_automated_bouton_analysis( record)
 
-
 % see tp_analyse_neurites
 measures = record.measures;
-ind_axons = [measures.axon];  % maar nu de axonen
+ROIlist = record.ROIs.celllist;
+indices = [measures.index]; %indices of all rois
 
+axons = find([measures.axon]);
+ind_axons = indices(axons);  % just the axons
 
+%ObjPos = [];
+for i = 1:length(axons) % over axons
+    axon = axons(i);
+    axoni = ind_axons(i);   %axon index number 
+    boutons = find(([measures.linked2neurite]==axoni) & [measures.bouton]);
+    axon_ints = find(([measures.linked2neurite]==axoni) & [measures.axon_int]);
+    
+   % ObjPos(i).axonidx = axoni;
+   %find closest spot on axon for each bouton +and take this position as position order
+   axi = ROIlist(axon).xi;
+   axj = ROIlist(axon).yi;
+    for j = 1:length(boutons)
+        bouton = boutons(j);
+        %position of this bouton
+        bi = mean(ROIlist(bouton).xi);
+        bj = mean(ROIlist(bouton).yi);  
 
-for i = ind_axons % over axons
-    axon = measures(i);
+        [~, posindx] = min((axi - bi).*(axi - bi) + (axj - bj).*(axj - bj));
+        ROIlist(bouton).extra = posindx;
+       % ObjPos(i).BoutonPos(j).pos = posindx; %position along the axon
+       % plot(ROIlist(axon).xi(posindx), ROIlist(axon).yi(posindx), '*')
+        
+    end
+    %find closest spot on axon for each axon_int +and take this position as position order
+    for j = 1:length(axon_ints)
+        ax_int = axon_ints(j);
+        %position of this bouton
+        ai = mean(ROIlist(ax_int).xi);
+        aj = mean(ROIlist(ax_int).yi);  
 
-    ind_linked2axon = ([measures.linked2neurite]==axon.index) & [measures.bouton] ;
+        [~, posindx] = min((axi - ai).*(axi - ai) + (axj - aj).*(axj - aj));
+        %ObjPos(i).AxintPos(j).pos = posindx; %position along the axon   
+        ROIlist(ax_int).extra = posindx;
+    end
     
 end
+
+record.ROIs.celllist = ROIlist;
+
+
+%now we have ordered boutons and axonints in space, take two axon ints
+%besides each bouton and calculate fractional response of bouton.
+
+
+
+
+
+
+    
+
