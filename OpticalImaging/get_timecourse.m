df8a0f9db131dd767e79b921ea9aeb25f8faf3be
kuppieknaus <laurarigter@gmail.com>
2015-06-08 11:48:19 +0200

    polished get_timecourse

--- OpticalImaging/get_timecourse.m
+++ OpticalImaging/get_timecourse.m
@@ -1,72 +1,61 @@
 function [tc_roi,tc_ror,ratio]=get_timecourse(fname,n_images,conditions,...
-				       roi,ror,compression,show)
+    roi,ror,compression,show)
 %GET_TIMECOURSE reads the timecourse over a subset of pixels
 %
 %
 %  [TC_ROI,TC_ROR,RATIO]=GET_TIMECOURSE(FNAME,N_IMAGES,CONDITIONS,...
 %				       ROI,ROR,COMPRESSION,SHOW)
-%        calculates the timecourse of file FNAME 
-%  
-%  
-%   2004, Alexander HeimelR
+%        calculates the timecourse of file FNAME
 %
-%   2005-01-02 JFH: Changed argument order and added ROR as argument
+%
+%   2004-2015, Alexander Heimel
+%
 
-if nargin<7
-  show=[];
+if nargin<7 || isempty(show)
+    show=1;
 end
-
 if nargin<6
-  compression=1;
+    compression=1;
 end
 if nargin<5
-  ror=[];
+    ror=[];
 end
 if nargin<4
-  roi=[];
+    roi=[];
 end
 if nargin<3
-  conditions=[];
+    conditions=[];
 end
 if nargin<2
-  n_images=inf;
+    n_images=inf;
 end
 tc_roi=[];
 tc_ror=[];
 ratio=[];
 
-if isempty(show)
-  show=1;
-end
-
-
-
-%disp('# Reading timecourse');
-%fname=complete_oi_path(fname);
-  
 % get file info
 fileinfo=imagefile_info(fname);
 if isempty(fileinfo.name)
-  return
+    return
 end
-  
+
 if fileinfo.n_images<n_images
-  n_images=fileinfo.n_images;
+    n_images=fileinfo.n_images;
 end
-  
-if isempty(ror) & ~isempty(roi)
-  % make ROR complement of ROI
-  ror=ones( size(roi))-roi;
+
+if isempty(ror) && ~isempty(roi)
+    % make ROR complement of ROI
+    ror=ones( size(roi))-roi;
 end
-if isempty(roi) 
-  % make ROI whole image
-  roi=ones( floor(fileinfo.xsize/compression), ...
-	    floor(fileinfo.ysize/compression) );
+if isempty(roi)
+    % make ROI whole image
+    roi=ones( floor(fileinfo.xsize/compression), ...
+        floor(fileinfo.ysize/compression) );
 end
-if isempty(ror) 
-  % make ROR also whole image
-  ror=ones( floor(fileinfo.xsize/compression), ...
-	    floor(fileinfo.ysize/compression) );
+if isempty(ror)
+    % make ROR also whole image
+    ror=ones( floor(fileinfo.xsize/compression), ...
+        floor(fileinfo.ysize/compression) );
 end
 pixelind_roi=find(roi'~=0);
 pixelind_ror=find(ror'~=0);
@@ -74,89 +63,75 @@ pixelind_ror=find(ror'~=0);
 % calculate number of batches
 memory_max=64*1024*1024;
 n_batches=ceil(fileinfo.filesize/(compression^2)/ ...
-	       memory_max/fileinfo.n_conditions);
+    memory_max/fileinfo.n_conditions);
 
 n_frames_per_batch=ceil(n_images/n_batches);
 
 if isempty(conditions)
-  conditions=(1:fileinfo.n_conditions);
+    conditions=(1:fileinfo.n_conditions);
 end
 
 tc_roi=zeros(n_images,length(conditions));
 tc_ror=zeros(n_images,length(conditions));
 
 for cond=conditions
-  % disp(['Reading condition: ' num2str(cond)]);
-  condoffset=(cond-1)*fileinfo.n_images;
-  start=1;
-  for i=1:n_batches
-
-
-    %fprintf(['Working on batch ' num2str(i) ' of ' num2str(n_batches)  ...
-		%    ' : ']);
-    
-    frames=read_oi_compressed(fname,start+condoffset,...
-			      n_frames_per_batch,1,compression,0);
-    
-    if isempty(frames)
-      disp(['Error: reading frames from ' fname ]);
-      return
+    condoffset=(cond-1)*fileinfo.n_images;
+    start=1;
+    for i=1:n_batches
+        frames=read_oi_compressed(fname,start+condoffset,...
+            n_frames_per_batch,1,compression,0);
+        
+        if isempty(frames)
+            errormsg(['Reading frames from ' fname ]);
+            return
+        end
+        
+        n_frames_read=size(frames,3);
+        frames=reshape(frames,size(frames,1)*size(frames,2),size(frames,3));
+        
+        if ~isempty(pixelind_roi)
+            avg_roi=mean(frames(pixelind_roi,:),1)';
+        else
+            avg_roi=mean(frames(:,:),1)';
+        end
+        
+        if ~isempty(pixelind_ror)
+            avg_ror=mean(frames(pixelind_ror,:),1)';
+        else
+            avg_ror=0*avg_roi+mean(frames(:));
+        end
+        
+        tc_roi(start:start+n_frames_read-1,cond)=avg_roi;
+        tc_ror(start:start+n_frames_read-1,cond)=avg_ror;
+        start=start+n_frames_read;
     end
-      
-    n_frames_read=size(frames,3);
-    
-    frames=reshape(frames,size(frames,1)*size(frames,2),size(frames,3));
-    
-    if ~isempty(pixelind_roi)
-      avg_roi=mean(frames(pixelind_roi,:),1)';
-    else
-      avg_roi=mean(frames(:,:),1)';
-    end
-
-    if ~isempty(pixelind_ror)
-      avg_ror=mean(frames(pixelind_ror,:),1)';
-    else
-      disp(['IF A ROUTINE USES THIS, THE BEHAVIOR MAY BE ALTERED NOW' ...
-	    ' 22 FEB 2006']);
-     %old behaviour:  avg_ror=mean(frames(:,:),1)';
-      avg_ror=0*avg_roi+mean(frames(:));
-    end
-
-    
-    tc_roi(start:start+n_frames_read-1,cond)=avg_roi;
-    tc_ror(start:start+n_frames_read-1,cond)=avg_ror;
-    start=start+n_frames_read;
- %   fprintf('\r');
-  end
 end
 
 ratio=tc_roi./tc_ror;
 
 if show==1
-  figure
-  subplot(3,1,1);
-  plot(tc_roi,'.-');
-  for i=1:size(tc_roi,2)
-    leg(i,:)=['stim ' char(i+48)];
-  end
-  legend(leg,-1);
-  title('ROI');
-
-  subplot(3,1,2);
-  plot(tc_ror,'.-');
-  for i=1:size(tc_ror,2)
-    leg(i,:)=['stim ' char(i+48)];
-  end
-  legend(leg,-1);
-  title('ROR');
-
-  subplot(3,1,3);
-  plot(ratio,'.-');
-  for i=1:size(tc_roi,2)
-    leg(i,:)=['stim ' char(i+48)];
-  end
-  legend(leg,-1);
-  title('ROI/ROR');
-
-
+    figure
+    subplot(3,1,1);
+    plot(tc_roi,'.-');
+    for i=1:size(tc_roi,2)
+        leg(i,:)=['stim ' char(i+48)];
+    end
+    legend(leg,-1);
+    title('ROI');
+    
+    subplot(3,1,2);
+    plot(tc_ror,'.-');
+    for i=1:size(tc_ror,2)
+        leg(i,:)=['stim ' char(i+48)];
+    end
+    legend(leg,-1);
+    title('ROR');
+    
+    subplot(3,1,3);
+    plot(ratio,'.-');
+    for i=1:size(tc_roi,2)
+        leg(i,:)=['stim ' char(i+48)];
+    end
+    legend(leg,-1);
+    title('ROI/ROR');
 end
