5d1a0f4e6ab9a9559dbdf041f1c32ab3dc98642f
heimel <heimel@nin.knaw.nl>
2015-10-25 23:46:19 +0100

    did some work on tp_mito_close, probably just to speed up

--- TwoPhoton/tp_mito_close.m
+++ TwoPhoton/tp_mito_close.m
@@ -34,18 +34,24 @@ for i = 1:n_rois
 %     r(i,1) = median(roilist(i).xi); % take center
 %     r(i,2) = median(roilist(i).yi); % take center
 %     r(i,3) = median(roilist(i).zi); % take center
-    r(i,1) = mean(roilist(i).xi); % take center
-    r(i,2) = mean(roilist(i).yi); % take center
-    r(i,3) = mean(roilist(i).zi); % take center
+%    r(i,1) = mean(roilist(i).xi); % take center
+%     r(i,2) = mean(roilist(i).yi); % take center
+%     r(i,3) = mean(roilist(i).zi); % take center
+    r(i,1) = sum(roilist(i).xi)/length(roilist(i).xi); % take center
+    r(i,2) = sum(roilist(i).yi)/length(roilist(i).yi); % take center
+    r(i,3) = sum(roilist(i).zi)/length(roilist(i).zi); % take center
 end
 
 r = r.*repmat([params.x_step params.y_step params.z_step],size(r,1),1); 
 
+
+ind_mito_present = ind_mito([roilist(ind_mito).present]==1);
+
 for i = ind_no_mito(:)'
     record.measures(i).mito_close = false;
     record.measures(i).distance2mito = inf;
-    for j = ind_mito(:)'
-       if  roilist(j).present && roilist(j).neurite(1)==roilist(i).neurite(1)
+    for j = ind_mito_present(:)'
+       if roilist(j).neurite(1)==roilist(i).neurite(1)
            record.measures(i).distance2mito  = ...
                min(record.measures(i).distance2mito, norm(r(i,:)-r(j,:)) );
        end
@@ -58,7 +64,8 @@ for i = ind_no_mito(:)'
     end
 end
 
-for i = ind_mito(:)'
+% set mito close true for mito's themselves
+for i = ind_mito(:)' 
     record.measures(i).mito_close = true;
     record.measures(i).distance2mito = 0;
 end
