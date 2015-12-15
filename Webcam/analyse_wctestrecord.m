01ac07edcd1e545a48fc6f29e6c115347fe98b78
Ta87 <azadeh.tafreshiha@gmail.com>
2015-12-15 16:30:50 +0100

    added tracking to repository

--- Webcam/analyse_wctestrecord.m
+++ Webcam/analyse_wctestrecord.m
@@ -13,10 +13,29 @@ if isempty(verbose)
     verbose = true;
 end
 
+par = wcprocessparams( record );
+
+
 wcinfo = wc_getmovieinfo( record);
 filename = fullfile(wcinfo.path,wcinfo.mp4name);
 
-[freezeTimes, flightTimes] = trackmouse(filename,true)
+stimsfile = getstimsfile(record);
+ stims = get(stimsfile.saveScript);
+ 
+ if length(stims)>1
+     warning('Only implemented for one stim at the time');
+ end
+
+ stimparams = getparameters(stims{1});
+ startside = stimparams.start_position;
+ rec = 1;
+% stimStart = (wcinfo(rec).stimstart-par.wc_playbackpretime) * 1.015
+
+ stimStart = round(wcinfo(rec).stimstart * 1.015*100)/100 ;%-1.2; %Factor for fixing delay (1/015) -0.8 correction %1.01445 more precisely %(stimstart*1.01445);
+
+ 
+[freezeTimes, flightTimes, pos_theta, head_theta, approach] = ...
+    trackmouseblack_pi(filename,false,stimStart,startside)
 
 record.measures.freezeTimes = freezeTimes;
 record.measures.flightTimes = freezeTimes;
