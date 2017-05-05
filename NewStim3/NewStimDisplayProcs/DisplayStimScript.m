2b27e173081e1ccdfd5ea3ea1ee07e01aec38b05
Alexander Heimel <alexander.heimel@gmail.com>
2017-05-05 15:15:14 +0200

    explained how to create newstim movie files

--- NewStim3/NewStimDisplayProcs/DisplayStimScript.m
+++ NewStim3/NewStimDisplayProcs/DisplayStimScript.m
@@ -26,13 +26,13 @@ function [MTI,startTrig] = DisplayStimScript(stimScript, MTI, priorit,abtable,ca
 %  Sound sometimes has errors if it is not run at priority 0.
 %
 %  200X, Steve Van Hooser
-%  200X-20017adapted by Alexander Heimel 200X-20017
+%  200X-20017, Alexander Heimel 
 
 NewStimGlobals;
 StimWindowGlobals;
 StimTriggerOpen;
 
-if nargin<5 || isempty(capture_movie); 
+if nargin<5 || isempty(capture_movie)
     capture_movie = false; 
 end
 
@@ -43,11 +43,13 @@ end
 prioritylevel = MaxPriority(StimWindowMonitor,'WaitBlanking','SetClut','GetSecs'); % PD
 
 if nargin>=3
-    if ~isempty(priorit),
+    if ~isempty(priorit)
         prioritylevel = priorit;
     else % set priority level more carefully if sound is going to be played
-        for i=1:length(MTI),
-            if strcmp(MTI{i}.ds.displayType,'Sound'),prioritylevel=0; end;
+        for i=1:length(MTI)
+            if strcmp(MTI{i}.ds.displayType,'Sound')
+                prioritylevel=0; 
+            end
         end
     end
 end
@@ -70,17 +72,27 @@ StimTriggerAct('Trigger_Initialize');
 HideCursor;
 ShowStimScreen;  % make sure screen is up
 
+if capture_movie && usejava('jvm')
+    disp('Recording a movie while JAVA capabilities is turned on is not supported by');
+    disp('Psychtoolbox and gstreamer. Start matlab without java by typing matlab -nojvm');
+    disp('in a command window. Then load your script (e.g. ps) and at the matlab prompt');
+    disp('type NSCaptureMovie(ps) to write a movie to your current folder.');
+    capture_movie = false;
+end
+
 if capture_movie
     disp('DISPLAYSTIMULUS: Creating movie. This will be slow!');
-    moviefilename = fullfile(getdesktopfolder,'stimulus_movie.mov');
+    moviefilename = fullfile(['"' getdesktopfolder '"'],'stimulus_movie.mov') ;
+    moviefilename = 'stimulus_movie.mov' ;
     try
+       % movieOptions =   ':CodecType=VideoCodec=x264enc speed-preset=1 noise-reduction=100000 ::: AudioCodec=faac ::: AudioSource=pulsesrc ::: Muxer=avimux';
+        movieOptions =   '';
         moviePtr = Screen('CreateMovie', StimWindow, moviefilename,[],[],StimWindowRefresh);
     catch
         disp('DISPLAYSTIMULUS: If codec missing on linux, run: sudo apt-get install ubuntu-restricted-extras gstreamer-tools');
         disp('DISPLAYSTIMULUS: Use gst-inspect to check which codecs are installed');
     end
 end
-
 Screen('screens'); 
 try % warm up these functions, try to get them in memory
     Snd('open'); 
@@ -108,7 +120,7 @@ end
 l = length(MTI);
 trigger = getTrigger(stimScript); %#ok<NASGU>
 
-if ~abortable,
+if ~abortable
     Rush('for i=1:l, [MTI{i}.startStopTimes,ft] = DisplayStimulus(MTI{i},get(stimScript,MTI{i}.stimid),trigger(i),capture_movie); MTI{i}.frameTimes = ft; end; Screen(StimWindow,''FillRect'',0);', prioritylevel);
 else
     if StimDisplayOrderRemote
@@ -134,10 +146,10 @@ else
             if KbCheck % abort if keyboard press
                 break
             end
-            if mod(i,20)==0,
+            if mod(i,20)==0
                 fprintf(['Just finished stim ' int2str(i) ' of ' int2str(l) '.\n']);
             end
-            if mod(i,numstims)==0,
+            if mod(i,numstims)==0
                 fprintf(['Just finished trial ' int2str(i/numstims) ' of ' int2str(l/numstims) '.\n']);
             end
         end
