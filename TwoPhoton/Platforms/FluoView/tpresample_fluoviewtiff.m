98b54168f0a43e05ff3f5a88cce884534a0e34fd
Alexander Heimel <alexander.heimel@gmail.com>
2017-10-06 14:57:44 +0200

    removed record as argument to tpresample_fluoviewtiff

--- TwoPhoton/Platforms/FluoView/tpresample_fluoviewtiff.m
+++ TwoPhoton/Platforms/FluoView/tpresample_fluoviewtiff.m
@@ -1,36 +1,41 @@
-function tpresample_fluoviewtiff( record)
-% TPRESAMPLE_FLUOVIEWTIFF squeezes a z-stack by average two consecutive frames 
+function tpresample_fluoviewtiff( fname)
+% TPRESAMPLE_FLUOVIEWTIFF squeezes a z-stack by average two consecutive frames
 %
 % 2017, Alexander Heimel
 %
 
-if nargin<1 || isempty(record)
+if nargin<1 
     experiment('examples');
     host('wall-e')
     db = load_testdb('tp');
-    record = db(1);
+    record = db(2);
+    fname = tpfilename(record);
 end
 
-iminf = tpreadconfig(record);
+iminf = tiffinfo(fname );
 
 if ~strcmp(iminf.third_axis_name,'Z')
     logmsg('Not a Z-stack');
     return
 end
 
-im = tpreadframe(record,[],[],[],[],[],3);
+im = zeros(iminf.Height,iminf.Width,iminf.NumberOfFrames,iminf.NumberOfChannels,'uint16');
+
+for ch = 1:iminf.NumberOfChannels
+    for fr = 1:iminf.NumberOfFrames
+        im(:,:,fr,ch)=imread(fname,(ch-1)*iminf.NumberOfFrames+fr);
+    end
+end
+
 
 orgclass = class(im);
 
 im = double(im); % to allow averaging
-if mod(iminf.number_of_frames,2)==1 % i.e. odd
+if mod(iminf.NumberOfFrames,2)==1 % i.e. odd
     % duplicate last frame
-    iminf.number_of_frames = iminf.number_of_frames +1;
     iminf.NumberOfFrames = iminf.NumberOfFrames +1;
     iminf.z(end+1) = iminf.z(end)+iminf.z_step;
-    iminf.frame_timestamp(end+1) = iminf.frame_timestamp(end)+iminf.frame_period;
-    iminf.frame_timestamp__us(end+1) = iminf.frame_timestamp__us(end)+iminf.frame_period__us;
-    im(:,:,end+1,:) = im(:,:,end,:); 
+    im(:,:,end+1,:) = im(:,:,end,:);
 end
 
 % average two consecutive frames
@@ -42,14 +47,10 @@ squeezed_im = squeezed_im /2 ;
 eval(['squeezed_im = ' orgclass '(squeezed_im);']);
 im = squeezed_im;
 
-iminf.number_of_frames = iminf.number_of_frames/2;
 iminf.NumberOfFrames = iminf.NumberOfFrames/2;
 iminf.z = (iminf.z(1:2:end)+iminf.z(2:2:end))/2;
-iminf.z_step = iminf.z_step*2; 
-iminf.frame_timestamp = (iminf.frame_timestamp(1:2:end)+iminf.frame_timestamp(2:2:end))/2;
-iminf.frame_timestamp__us = (iminf.frame_timestamp__us(1:2:end)+iminf.frame_timestamp__us(2:2:end))/2;
+iminf.z_step = iminf.z_step*2;
 
-fname = tpfilename(record);
 [pathstr,fname,ext] = fileparts(fname);
 
 fname = [fname '_squeezed'];
