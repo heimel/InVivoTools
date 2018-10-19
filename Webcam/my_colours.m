5c0dce6fff412e9aecc620a11618f76b245bbcc8
Azadeh <azadeh.tafreshiha@gmail.com>
2018-10-19 10:36:25 +0200

    Colours I use in graphs

---
+++ Webcam/my_colours.m
@@ -0,0 +1,27 @@
+% Loads colours used in grapgs
+% Source: http://www.tayloredmktg.com/rgb/
+% Azadeh September 2017
+my_rosybrown = [188/255 143/255 143/255];
+my_burlywood = [222/255 184/255 135/255];
+my_thistle = [216/255 191/255 216/255];
+my_purple = [0.4 0.0 0.4];
+my_green =  [0.2 0.6 0.4];
+grey_50 = [0.5 0.5 0.5];
+grey_30 = [0.3 0.3 0.3];
+dark_salmon = [233/255 150/255 122/255];
+light_salmon = [255/255 160/255 122/255];
+my_pink = [255/255 192/255 203/255];
+dark_khaki = [189/255 183/255 107/255];
+my_aquamarine = [127/255 205/255 170/255];
+my_sienna = [160/255 82/255 45/255];
+saddle_brown = [139/255 69/255 19/255];
+fire_brick = [178/255 34/255 34/255];
+my_brown = [165/255 42/255 42/255];
+
+% SfN poster
+my_burlywood = [222/255 184/255 135/255]; %habituating stim
+my_turquoiseblue = [64/255 224/255 208/255]; % novel stim
+my_peru = [205/255 133/255 63/255]; % hab durations
+my_darkturquoise = [0/255 206/255 209/255]; % novel durations
+my_wheat = [245/255 222/255 179/255]; % hawk as hab stim
+my_lightturquoise = [175/255 238/255 238/255]; %disc as hab stim
