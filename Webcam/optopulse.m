896056dcb7cce7c89a1bfcf33f6203ffdc8bd094
Alexander Heimel <alexander.heimel@gmail.com>
2018-12-04 09:57:34 +0100

    extended optopulse.m

--- Webcam/optopulse.m
+++ Webcam/optopulse.m
@@ -1,14 +1,28 @@
-function optopulse( duration)
+function optopulse( duration, frequency)
 %OPTOPULSE wrapper around calling optopulse.c
 %   should become mex file
+%
+%  OPTOPULSE( DURATION=60, FREQUENCY=20)
+%        DURATION in seconds
+%        FREQUENCY in Hz
+%    Use DURATION = 0 to kill previous optopulse
 %   
 %   2018, Alexander Heimel
 %
 
 if nargin<1 || isempty(duration)
-  system('~/Software/InVivoTools/Webcam/optopulse',false,'async');
-else
-  system(['~/Software/InVivoTools/Webcam/optopulse ' num2str(duration)],false,'async');
+    duration = 60;
+end
+if nargin<2 || isempty(frequency)
+    frequency = 20;
 end
 
+if duration==0
+    system('pkill optopulse');
+end
+
+cmd = '~/Software/InVivoTools/Webcam/optopulse';
+cmd = [cmd ' ' num2str(duration) ' ' num2str(frequency)];
+
+system(cmd,false,'async');
 
