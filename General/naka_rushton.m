79746b19f3fcdd505f831800b01810c97e8c5633
Alexander Heimel <alexander.heimel@gmail.com>
2019-01-21 12:33:24 +0100

    formatting naka_rushton

--- General/naka_rushton.m
+++ General/naka_rushton.m
@@ -21,77 +21,67 @@ function [rm,b,n,m,exitflag] = naka_rushton(c,data,xo)
 %    and used to fit contrast data of cortical cells in
 %    Albrecht and Hamilton, J. Neurophys. 48: 217-237, 1982
 %
-exitflag=[];
+% 200X-2019, Alexander Heimel
 
-if nargin<3;xo=[];end
+if nargin<3
+    xo = [];
+end
 
 if max(c)<=1 && min(c)>0.01
-	c=[0.01 c];
-	data=[0 data];
-    rescale_to_1 = false;
+    c = [0.01 c];
+    data = [0 data];
 elseif min(c)>1 % will rescale
-	c=[1 c];
-	data=[0 data];
-    rescale_to_1 = true;
+    c = [1 c];
+    data = [0 data];
     c = c/100;
 end
 
-
-
 % clip at maximum to remove supersaturation data
- [m,ind] = max(data);
- data(ind:end) = m;
-
+[m,ind] = max(data);
+data(ind:end) = m;
 
 if isempty(xo)
-  % initial conditions
-  xo = [ max(data(:)) 0.4];
-  if nargout>2
-    xo(3)=2;  % n (exponent)
-  end
-  if nargout>3
-    xo(4)=xo(3);  % m (exponent)
-  end
+    % initial conditions
+    xo = [ max(data(:)) 0.4];
+    if nargout>2
+        xo(3) = 2;  % n (exponent)
+    end
+    if nargout>3
+        xo(4) = xo(3);  % m (exponent)
+    end
 end
 
-matlabversion=version;
+options = optimset;
+options.MaxFunEvals = 10000;
+options.MaxIter = 10000;
+options.TolFun = 1e-2;
+options.TolX = 1e-2;
+%eval('[x,fval,exitflag] = fminsearch(@(x) naka_rushton_err(x,c,data),xo,options);');
 
-switch matlabversion(1:3)
- case '5.3',
-		options=foptions; %#ok<FDEPR>
-		options(1)=0;
-		options(2)=1e-6;
-		eval('[x] = fmins(''naka_rushton_err'',xo,options,[],c,data)');
-	otherwise
-		options=optimset;
-%    options.Display = 'final';
-        options.MaxFunEvals=10000;
-    options.MaxIter=10000;
-    options.TolFun = 1e-2;
-    options.TolX = 1e-2;
-		eval('[x,fval,exitflag] = fminsearch(@(x) naka_rushton_err(x,c,data),xo,options);');
-end
+[x,~,exitflag] = fminsearch(@(x) naka_rushton_err(x,c,data),xo,options);
 
-rm=x(1);b=x(2);
+rm = x(1);
+b = x(2);
+
 if nargout>2
-	n=x(3);
+    n = x(3);
 end
 if nargout>3
-	m=x(4);
+    m = x(4);
 end
 
 if rm<0
-  rm=max(abs(data(:)))*10^-8;
-  b=10*max(c);
+    rm = max(abs(data(:)))*10^-8;
+    b = 10*max(c);
 end
 
 if 0
- figure;
- plot(c,data,'+');
- hold on
- cn=(0:0.01:1);
- r=rm* (cn.^n)./ (b^n+cn.^n) ; % without spont
- plot(cn,r);
+    figure; %#ok<UNRCH>
+    plot(c,data,'+');
+    hold on
+    cn = (0:0.01:1);
+    r = rm* (cn.^n)./ (b^n+cn.^n) ; % without spont
+    plot(cn,r);
 end
 
 
