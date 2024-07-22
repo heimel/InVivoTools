0739e33e395b8375cf8b371e68077f1e44c56817
Alexander Heimel <alexander.heimel@gmail.com>
2024-07-22 18:00:07 +0200

    Improved close_figs and added empty databases

--- General/graphs/close_figs.m
+++ General/graphs/close_figs.m
@@ -3,18 +3,16 @@ function close_figs
 %
 %  CLOSE_FIGS
 %
-% 2007, Alexander Heimel
+% 2007-2024, Alexander Heimel
 %
 
-c=get(0,'Children');
-for i=1:length(c)
-	cb='';
-	try
-		fud=get(c(i),'UserData');
-		if fud.persistent==0
-			close(c(i));
-	end
-	catch
-		close(c(i));
-	end
+c = get(0,'Children');
+for i = 1:length(c)
+    if isa(c(i),'matlab.ui.Figure')
+        fud = get(c(i),'UserData');
+        if isempty(fud) || ~isfield(fud,'persistent') || ~fud.persistent
+            close(c(i));
+            delete(c(i));
+        end
+    end
 end
