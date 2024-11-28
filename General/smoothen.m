3fae4adabe3f6bc12eff618ba489f50f1d105323
Alexander Heimel <alexander.heimel@gmail.com>
2024-11-28 20:56:16 +0100

    removed smooth (original in matlab)

--- General/smoothen.m
+++ General/smoothen.m
@@ -1,30 +1,33 @@
-function y=smoothen(x,sigma)
+function y = smoothen(x,sigma)
 %SMOOTHEN convolves vector with gaussian
 %
-%   Y=SMOOTHEN(X,SIGMA)
+%   Y = SMOOTHEN(X,[SIGMA=1])
 %        X can be 1D or 2D
 %
-%  2004, Alexander Heimel
+%  2004-2024, Alexander Heimel
 
+if nargin<2 || isempty(sigma)
+    sigma = 1;
+end
 if isnan(sigma) || sigma==0
     y = x;
     return
 end
 
-cutoff=ceil(3*sigma);
+cutoff = ceil(3*sigma);
 
 if numel(x)==length(x) % 1D
-    gauss=(-cutoff:cutoff);
+    gauss = (-cutoff:cutoff);
     if sigma>0
-        gauss=exp(- gauss.^2/2/sigma^2);
+        gauss = exp(- gauss.^2/2/sigma^2);
     else
-        gauss=1;
+        gauss = 1;
     end
     
-    gauss=gauss/sum(gauss);
+    gauss = gauss/sum(gauss);
     
-    y=conv(x,gauss);
-    y=y(cutoff+1:end-cutoff);
+    y = conv(x,gauss);
+    y = y(cutoff+1:end-cutoff);
     return
 end
 
