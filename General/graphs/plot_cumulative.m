053c3ff95d40190bc35d9e3f55a1caf690daf304
Alexander Heimel <alexander.heimel@gmail.com>
2026-04-10 22:31:51 +0200

    Change sem to ivt_sem in plot_cumulative

--- General/graphs/plot_cumulative.m
+++ General/graphs/plot_cumulative.m
@@ -10,74 +10,74 @@ function [cf_mean,histbin_centers,h]=plot_cumulative( r,bins,color,showerrors,pr
 
 if nargin<6;remove_nan=[];end
 if isempty(remove_nan)
-	remove_nan=0;
+    remove_nan=0;
 end
 if nargin<5;prefax=[];end
 if nargin<4;showerrors=[];end
 if isempty(showerrors)
-	showerrors=1;
+    showerrors=1;
 end
 if nargin<3
-  color='b';
+    color='b';
 end
 if nargin<2;bins=[];end
 if isempty(bins)
-	bins=10;
+    bins=10;
 end
 
 if ~iscell(r)
-  r={r};
+    r={r};
 end
 
 if remove_nan
-	for i=1:length(r)
-		r{i}=r{i}(~isnan(r{i}));
-	end
+    for i=1:length(r)
+        r{i}=r{i}(~isnan(r{i}));
+    end
 end
 
 r_pooled=[r{:}];
 [n,histbin_centers]=hist(r_pooled,bins );
 
 for c=1:length(r)
-  n_hist(:,c)=hist(r{c},histbin_centers);
-  f_hist(:,c)=n_hist(:,c)/length(r{c}); % compute fraction
-  r_min(c)=min(r{c});
+    n_hist(:,c)=hist(r{c},histbin_centers);
+    f_hist(:,c)=n_hist(:,c)/length(r{c}); % compute fraction
+    r_min(c)=min(r{c});
 end
 cf=cumsum(f_hist);
 cf_mean=mean(cf,2);
 
 hold on;
 if ischar(color)
-  tempcolor=color;
+    tempcolor=color;
 else
-  tempcolor='b';
+    tempcolor='b';
 end
 
 h=plot([mean(r_min); histbin_centers'],[mean(zeros(1,length(r)));cf_mean],tempcolor);
 %h=plot([ histbin_centers'],[cf_mean],tempcolor);
 if ~ischar(color)
-  set(h,'Color',color)
+    set(h,'Color',color)
 end
 
 if isempty(prefax)
-	ax=axis;
-	axis( [ax([1 2]) 0 1]);
+    ax=axis;
+    axis( [ax([1 2]) 0 1]);
 end
 
 if ~isempty(prefax)
-  if length(prefax)==2
-    axis( [ prefax 0 1]);
-  else
-    axis(prefax);
-  end
+    if length(prefax)==2
+        axis( [ prefax 0 1]);
+    else
+        axis(prefax);
+    end
 end
 
 ax=axis;
 if length(r)>1 && showerrors==1
-  cf_sem=sem(cf,2);
-  errind=fix(linspace(findclosest(histbin_centers,ax(1)),...
-    findclosest(histbin_centers,ax(2)),7));
-  errind=errind(2:end-1);
-  errorbar(histbin_centers(errind),cf_mean(errind),cf_sem(errind),['.' color]);
+    cf_sem = ivt_sem(cf,2);
+    errind = fix(linspace(findclosest(histbin_centers,ax(1)),...
+        findclosest(histbin_centers,ax(2)),7));
+    errind = errind(2:end-1);
+    errorbar(histbin_centers(errind),cf_mean(errind),cf_sem(errind),['.' color]);
 end
 hold off;
